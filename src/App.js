5e4b02b4e3a68d8327bf570b026bcffc21592063
FerhatUcar <ucarferhat3@gmail.com>
2020-01-19 19:41:23 +0100

    adds provider to the app with the store as a prop

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, {useEffect, Fragment} from 'react';
+
+// style
 import './App.scss';
 
+// redux
+import {Provider} from 'react-redux';
+import store from "./store";
+
+// modals
 import AddLogModal from './components/logs/AddLogModal';
 import EditLogModal from './components/logs/EditLogModal';
 import AddTechModal from './components/techs/AddTechModal';
 import TechListModal from './components/techs/TechListModal';
 
+// material design
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
+// components
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from './components/layout/AddBtn';
 
+// app
 const App = () => {
     useEffect(() => {
         // Init Materialize JS
@@ -20,17 +30,19 @@ const App = () => {
     });
 
     return (
-        <Fragment>
-            <SearchBar/>
-            <div className="container">
-                <AddBtn />
-                <AddLogModal />
-                <EditLogModal />
-                <AddTechModal />
-                <TechListModal />
-                <Logs/>
-            </div>
-        </Fragment>
+        <Provider store={store}>
+            <Fragment>
+                <SearchBar/>
+                <div className="container">
+                    <AddBtn />
+                    <AddLogModal />
+                    <EditLogModal />
+                    <AddTechModal />
+                    <TechListModal />
+                    <Logs/>
+                </div>
+            </Fragment>
+        </Provider>
     );
 };
 
